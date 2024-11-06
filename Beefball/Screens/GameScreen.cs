10892cb56b492c9a6b2b5266fea37488713f2a89
Ryszard Pyka <60892050+RychuP@users.noreply.github.com>
2024-11-06 06:30:59 +0000

    Add project files.

---
+++ Beefball/Screens/GameScreen.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+using FlatRedBall;
+using FlatRedBall.Input;
+using FlatRedBall.Instructions;
+using FlatRedBall.AI.Pathfinding;
+using FlatRedBall.Graphics.Animation;
+using FlatRedBall.Gui;
+using FlatRedBall.Math;
+using FlatRedBall.Math.Geometry;
+using FlatRedBall.Localization;
+using Microsoft.Xna.Framework;
+
+using Beefball.Entities;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace Beefball.Screens
+{
+    public partial class GameScreen
+    {
+
+        void CustomInitialize()
+        {
+            AssignInput();
+
+        }
+
+        void CustomActivity(bool firstTimeCalled)
+        {
+
+
+        }
+
+        void CustomDestroy()
+        {
+
+
+        }
+
+        static void CustomLoadStaticContent(string contentManagerName)
+        {
+
+
+        }
+
+        void AssignInput()
+        {
+            PlayerBall1.MovementInput = InputManager.Keyboard.Get2DInput(
+                Keys.A, Keys.D, Keys.W, Keys.S);
+            PlayerBall1.BoostInput = InputManager.Keyboard.GetKey(Keys.F);
+        }
+    }
+}
